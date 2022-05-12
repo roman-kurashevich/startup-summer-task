d2eda168258c82ccbcb7801251ad4014ec866b6e
Roman Kurashevich <rkurashevich@gmail.com>
2022-05-12 14:28:53 +0800

    feat: remove lock for screen orientation

--- src/index.tsx
+++ src/index.tsx
@@ -6,14 +6,6 @@ import App from "./App";
 import store from "./redux/store";
 import "./index.scss";
 
-if (
-  navigator.userAgent.toLowerCase().indexOf("iphone") > -1 ||
-  navigator.userAgent.toLowerCase().indexOf("android") > -1 ||
-  navigator.userAgent.toLowerCase().indexOf("webos") > -1
-) {
-  window.screen.orientation.lock("portrait");
-}
-
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Failed to find the root element");
 const root = ReactDOM.createRoot(rootElement);
